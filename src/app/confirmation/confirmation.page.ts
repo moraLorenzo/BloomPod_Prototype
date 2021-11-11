3e0136271600f4b7024bea486f85df9656cad43d
moraLorenzo <201810219@gordoncollege.edu.ph>
2021-11-11 13:08:36 +0800

    Confirmation

--- src/app/confirmation/confirmation.page.ts
+++ src/app/confirmation/confirmation.page.ts
@@ -11,28 +11,52 @@ export class ConfirmationPage implements OnInit {
   bouquet_obj: any;
   content: any;
   total: number = 0;
+
+  primary: any;
+  secondary: any;
+  tertiary: any;
+
+  primary_price: any;
+  secondary_price: any;
+  tertiary_price: any;
+
+  quantity: any;
   constructor(public router: Router, private fs: FlowersService) {}
 
   ngOnInit() {}
 
   profile() {
-    this.router.navigate(['tab4']);
+    this.router.navigate(['tabs/tab4']);
   }
 
   ionViewWillEnter() {
     // console.log(history.state.data);
     this.bouquet_obj = history.state.data;
+
+    this.quantity = this.bouquet_obj.quantity;
+    this.primary = this.bouquet_obj.primary;
+    this.secondary = this.bouquet_obj.secondary;
+    this.tertiary = this.bouquet_obj.tertiary;
+
+    this.primary_price = this.bouquet_obj.primary_price;
+    this.secondary_price = this.bouquet_obj.secondary_price;
+    this.tertiary_price = this.bouquet_obj.tertiary_price;
+
     console.log(this.bouquet_obj);
     if (this.bouquet_obj.quantity == 6) {
       this.content = this.fs.six(
         this.bouquet_obj.primary,
         this.bouquet_obj.secondary
       );
+      this.total = this.bouquet_obj.primary_price * 3;
+      this.total += +this.bouquet_obj.secondary_price * 3;
     } else if (this.bouquet_obj.quantity == 9) {
       this.content = this.fs.nine(
         this.bouquet_obj.primary,
         this.bouquet_obj.secondary
       );
+      this.total = this.bouquet_obj.primary_price * 3;
+      this.total += +this.bouquet_obj.secondary_price * 6;
     } else if (this.bouquet_obj.quantity == 12) {
       this.content = this.fs.twelve(this.bouquet_obj.primary, [
         this.bouquet_obj.secondary,
